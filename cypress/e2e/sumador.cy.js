e28dbba44162508a1ce3dd3d0ee97b9f2ae1b5e5
Nicolas <nicolascari.rz@gmail.com>
2022-11-24 09:56:26 -0400

    agregando cypress

--- cypress/e2e/sumador.cy.js
+++ cypress/e2e/sumador.cy.js
@@ -1,6 +1,16 @@
-describe("Sumador", () => {
-  it("Shows the amount of the addition to the user", () => {
+describe("twitter", () => {
+  it("Shows you can tweet", () => {
     cy.visit("/");
-    cy.get("#primer-numero").type("hola");
+    cy.get("#cadena").type("que tal");
+  });
+  it("Shows you can click button", () => {
+    cy.visit("/");
+    cy.get("#addButton").click();
+  });
+  it("Shows list", () => {
+    cy.visit("/");
+    cy.get("#cadena").type("que tal");
+    cy.get("#addButton").click();
+    cy.get("#resultado").should('have.value',"que tal")
   });
 });
